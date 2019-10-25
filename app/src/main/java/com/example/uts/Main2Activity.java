bfb0e932c4c2478a1a71e02a7da19ca6d0be6c06
Yunita <yunitamaulidiapratiwi@gmail.com>
2019-10-25 20:19:28 +0700

    20.19

--- app/src/main/java/com/example/uts/Main2Activity.java
+++ app/src/main/java/com/example/uts/Main2Activity.java
@@ -2,7 +2,9 @@ package com.example.uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void handleregistrasi(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+
+    public void handlelogin(View view) {
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/uts/MainActivity.java
+++ app/src/main/java/com/example/uts/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private int delay=1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(home);
+                finish();
+            }
+        },delay);
     }
 }
